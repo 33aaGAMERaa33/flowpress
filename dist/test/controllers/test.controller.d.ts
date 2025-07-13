d7fc827613dcf967c732b86ed10f217e33a778d7
Luan Gabriel da Silva <137464815+33aaGAMERaa33@users.noreply.github.com>
2025-07-12 21:53:13 -0300

    tirando o test

--- dist/test/controllers/test.controller.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class TestController {
-    private readonly testService;
-    test(): Promise<{
-        status: string;
-        message: string;
-    }>;
-}
--- dist/test/entities/visities.entity.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class VisitiesEntity {
-    id?: number;
-    visited_at?: Date;
-}
