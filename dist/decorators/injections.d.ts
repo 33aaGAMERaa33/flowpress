f86843dee2b83ee0668627d39905c354f2b6f82c
Luan Gabriel da Silva <137464815+33aaGAMERaa33@users.noreply.github.com>
2025-07-15 00:07:56 -0300

    Update injections.d.ts

--- dist/decorators/injections.d.ts
+++ dist/decorators/injections.d.ts
@@ -1,5 +1,4 @@
-import { ClassConstructor } from "../defines/class_constructor";
 import { InjectionType } from "../enums/injection_type.enum";
 export type InjectDefine = [InjectionType, string | symbol, any];
 export declare const Inject: () => PropertyDecorator;
-export declare const InjectRepository: (entity: ClassConstructor) => PropertyDecorator;
+export declare const InjectRepository: (entity: Object) => PropertyDecorator;
