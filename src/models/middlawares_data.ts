bcaed97e5471880ece910a1ba11ea788ba18a288
Luan Gabriel da Silva <137464815+33aaGAMERaa33@users.noreply.github.com>
2025-07-17 19:58:30 -0300

    a

--- src/models/middlawares_data.ts
+++ src/models/middlawares_data.ts
@@ -1,12 +1,13 @@
 import { ORIGINAL_CONSTRUCTOR_METADATA_KEY } from "../constants/metadata_keys/original-constructor";
 import { ClassConstructor } from "../defines/class_constructor";
+import { GlobalMiddlewareImpl } from "../interfaces/global_middleware.impl";
 import { MiddlewareImpl } from "../interfaces/middleware.impl";
 import { MiddlewareData } from "./middleware_data";
 
 export class MiddlewaresDataContainer {
     readonly middlewaresData: MiddlewareData[] = [];
     
-    getMiddlawareData<T extends MiddlewareImpl>(middlewareConstructor: ClassConstructor<T>): MiddlewareData | undefined {
+    getMiddlawareData<T extends MiddlewareImpl | GlobalMiddlewareImpl>(middlewareConstructor: ClassConstructor<T>): MiddlewareData | undefined {
         const originalConstructor = Reflect.getMetadata(ORIGINAL_CONSTRUCTOR_METADATA_KEY, middlewareConstructor) ?? middlewareConstructor.constructor;
         
         let middlewareData: MiddlewareData | undefined = this.middlewaresData.find((middlewareData) => {
