32d1d327d1e11b3294e95fd8382fb891a6c73df1
Luan Gabriel da Silva <137464815+33aaGAMERaa33@users.noreply.github.com>
2025-07-17 19:56:05 -0300

    a

--- src/decorators/use_middleware.ts
+++ src/decorators/use_middleware.ts
@@ -1,9 +1,10 @@
 import { ClassConstructor } from "../defines/class_constructor";
 import { USE_MIDDLEWARE_METADATA_KEY } from "../constants/metadata_keys/use_middleware";
+import { MiddlewareImpl } from "../interfaces/middleware.impl";
 
 export type RoutesMiddlewares = Record<string | symbol, ClassConstructor[]>;
 
-export function UseMiddleware<T extends ClassConstructor>(middlewareConstructor: T, ): MethodDecorator {
+export function UseMiddleware<T extends ClassConstructor<MiddlewareImpl>>(middlewareConstructor: T, ): MethodDecorator {
     return function(target, propertyKey, _) {
         // Pega as definições de middlawares que vai ser usado
         const routesMiddlewares: RoutesMiddlewares = Reflect.getMetadata(USE_MIDDLEWARE_METADATA_KEY, target.constructor) ?? {};
